290740eafbb27e239b983be774d7e1c389cfb240
Sárközi Gergő <h164920@stud.u-szeged.hu>
2023-04-25 18:08:25 +0200

    gui fix

--- PhoneShop/app/src/main/java/hu/mobilalk/phoneshop/Adapters/CartAdapter.java
+++ PhoneShop/app/src/main/java/hu/mobilalk/phoneshop/Adapters/CartAdapter.java
@@ -5,6 +5,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
